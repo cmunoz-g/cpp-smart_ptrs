901059c061501edafe59c5a3a4d07240d674108f
Carlos <cmunozg.dev@gmail.com>
2025-10-13 17:23:26 +0200

    project setup

---
+++ include/shared_pointer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace ptrs {
+
+    template <typename T>
+    class control_block {
+    private:
+        T* ptr_;
+        uint64_t count_;
+
+    public:
+        
+    };
+
+    template <typename T>
+    class shared_ptr {
+        
+
+    };
+
+} /* ptrs */
