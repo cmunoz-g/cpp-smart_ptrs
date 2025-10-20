18f0f0322e75bbddaa9bd9973f92c5fe53c2b059
Carlos <cmunozg.dev@gmail.com>
2025-10-20 10:39:07 +0200

    substituted atomics for a mutex

--- include/control_block.hpp
+++ include/control_block.hpp
@@ -1,20 +1,24 @@
 #pragma once
 #include <atomic>
+#include <mutex>
 
 namespace smart_ptrs {
     template <typename T>
     class control_block_base { 
     protected:
-        std::atomic<size_t> strong_count_(1);
-        std::atomic<size_t> weak_count_(0);
+        std::mutex m;
+        size_t strong_count_(1);
+        size_t weak_count_(0);
 
         /* Increments, Decrements */
         void increment_strong() noexcept { 
-            strong_count_.fetch_add(1, std::memory_order_relaxed);
+            std::lock_guard<std::mutex> lg(m);
+            strong_count_++;
         }
-
+        
         void increment_weak() noexcept {
-            weak_count_.fetch_add(1, std::memory_order_relaxed);
+            std::lock_guard<std::mutex> lg(m);
+            weak_count_++;
         }
 
     public:
@@ -22,10 +26,10 @@ namespace smart_ptrs {
         virtual ~control_block() = default;
 
         long use_strong_count() const noexcept {
-            return strong_count_.load(std::memory_order_acquire);
+            std::lock_guard<std::mutex> lg(m);
+            return strong_count_;
         }
 
-        /* Both of these are wrong, there should be only one atomic operation*/
         virtual void release_strong(T* p) = 0;
         virtual void release_weak(T* p) = 0;
     };
@@ -34,18 +38,19 @@ namespace smart_ptrs {
     class control_block_separate final : public control_block_base {
     public:
         void release_strong(T* p) override {
-            if (strong_count_.load(std::memory_order_acquire) == 1) {
+            std::lock_guard<std::mutex> lg(m);
+            if (strong_count_ == 1) {
                 delete p;
-                if (weak_count_.load(std::memory_order_acquire) == 0) {
+                if (weak_count_ == 0) {
                     delete this;
                 }
             }
         }
 
         void release_weak(T* p) override {
-            if (weak_count_.load(std::memory_order_acquire) == 1 &&
-                strong_count_.load(std::memory_order_acquire) == 0) {
-                    delete this;
+            std::lock_guard<std::mutex> lg(m);
+            if (weak_count_ == 1 && strong_count_ == 0) {
+                delete this;
             }
         }
     };
@@ -54,19 +59,20 @@ namespace smart_ptrs {
     class control_block_adjacent final : public control_block_base {
     public:
         void release_strong(T* p) override {
-            if (strong_count_.load(std::memory_order_acquire) == 1) {
+            std::lock_guard<std::mutex> lg(m);
+            if (strong_count_ == 1) {
                 p->~T();
-                if (weak_count_.load(std::memory_order_acquire == 0)) {
+                if (weak_count_ == 0) {
                     ::operator delete(this); // will this free everything (data + cb) ?
                 }
             }
         }
 
         void release_weak(T* p) override {
-            if (weak_count_.load(std::memory_order_acquire) == 1 &&
-                strong_count_.load(std::memory_order_acquire) == 0) {
-                    ::operator delete(this);
-                }
+            std::lock_guard<std::mutex> lg(m);
+            if (weak_count_ == 1 && strong_count_ == 0) {
+                ::operator delete(this);
+            }
         }
     };
 
